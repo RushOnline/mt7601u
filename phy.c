ee676b0bcc1fcf28512cd8a90458594eeba461b0
Евгений Н <gigarush@ya.ru>
2022-09-03 07:33:51 +0300

    remove calibrations, thanks goes to:
    * https://www.spinics.net/lists/linux-wireless/msg210463.html
    * https://1it.xyz/poleznoe/linux/resheno-zapusk-wifi-na-dongle-ot-mediatek

--- phy.c
+++ phy.c
@@ -589,8 +589,6 @@ void mt7601u_phy_recalibrate_after_assoc(struct mt7601u_dev *dev)
 	if (test_bit(MT7601U_STATE_REMOVED, &dev->state))
 		return;
 
-	mt7601u_mcu_calibrate(dev, MCU_CAL_DPD, dev->curr_temp);
-
 	mt7601u_rxdc_cal(dev);
 }
 
@@ -1157,9 +1155,6 @@ static int mt7601u_init_cal(struct mt7601u_dev *dev)
 	if (ret)
 		return ret;
 	ret = mt7601u_mcu_calibrate(dev, MCU_CAL_RXIQ, 0);
-	if (ret)
-		return ret;
-	ret = mt7601u_mcu_calibrate(dev, MCU_CAL_DPD, dev->dpd_temp);
 	if (ret)
 		return ret;
 
